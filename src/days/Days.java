d7a58e95f2c8db83fafdaf98cae5e2a950662daa
thakuans <thakuans@sheridancollege.ca>
2023-10-23 15:48:09 -0400

    this is my code

---
+++ src/days/Days.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package days;
+
+
+
+/**This class takes String input from user 
+ * and calls method  to print the 
+ * name of the day.
+ * if the input is not given in String, 
+ * program will accept and produce wrong results. 
+ * change the code to use enums which avoids String input 
+ * then print the week day names. 
+ *
+ * @author sivagamasrinivasan
+ */
+
+
+
+
+
+/**This class takes String input from user 
+ * and calls method  to print the 
+ * name of the day.
+ * if the input is not given in String, 
+ * program will accept and produce wrong results. 
+ * change the code to use enums which avoids String input 
+ * then print the week day names. 
+ *
+ * @author sivagamasrinivasan
+ */
+
+public class Days {
+
+    public static void main(String[] args) {
+        System.out.println("All the day names:");
+        Weekdays[] weekdays = Weekdays.values();
+        for (Weekdays day : weekdays) {
+            System.out.println(day);
+        }
+    }
+}
---
+++ src/days/Weekdays.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package days;
+
+/**
+ *
+ * @author sivagamasrinivasan
+ */
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author sivagamasrinivasan
+ */
+
+
+public enum Weekdays {
+    ONE("Monday"),
+    TWO("Tuesday"),
+    THREE("Wednesday"),
+    FOUR("Thursday"),
+    FIVE("Friday"),
+    SIX("Saturday"),
+    SEVEN("Sunday");
+
+    private final String dayName;
+
+    Weekdays(String dayName) {
+        this.dayName = dayName;
+    }
+
+    @Override
+    public String toString() {
+        return dayName;
+    }
+}
